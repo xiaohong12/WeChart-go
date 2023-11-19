7c7c56152e03b6a0ea934d326fd79265de22697a
snake@horizonpay.cn <xiaohong@20>
2023-11-20 07:51:20 +0800

    1

--- miniprogram/pages/order/index.js
+++ miniprogram/pages/order/index.js
@@ -1,4 +1,30 @@
+import shopList from "../../api/order/shop-list"
 Page({
+data:{
+  top:0,
+  buttonInfo:{
+    top:0,
+    right:0
+  },
+  currentActiveType:1,
+  shopList:[]
+},
+setActiveType(e){
+  const type=e.target.dataset.type
+  this.setData({
+    currentActiveType:type,
+  })
+  },
+  
+onLoad(){
+ const info= wx.getMenuButtonBoundingClientRect() //获取胶囊的top值
+ console.log(info)
+ this.setData({
+  buttonInfo:info,
+  shopList:shopList
+ })
+},
+
     onShow() {
       if (typeof this.getTabBar === 'function' && this.getTabBar()) {
         this.getTabBar().setData({
