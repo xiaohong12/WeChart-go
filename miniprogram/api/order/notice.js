d578ac9c79576d2fdb3356b1e1894121ef990476
Snake <snake@horizonpay.cn>
2023-11-20 17:04:48 +0800

    add

---
+++ miniprogram/api/order/notice.js
@@ -0,0 +1,99 @@
+export default {
+		"shop_activity_image": "https://static.heytea.com/taro_trial/v1/img/index/v2/shop_notice_title.png",
+		"notice": [{
+			"id": 113,
+			"content": "领券，周一免配送费~",
+			"image_id": 1038515,
+			"image": {
+				"id": 1038515,
+				"path": "/storage/ad/2023/09/07/3248460945cc42b6b4906777c00dd098.jpg",
+				"url": "https://go.cdn.heytea.com/storage/ad/2023/09/07/3248460945cc42b6b4906777c00dd098.jpg",
+				"width": "144",
+				"height": "48",
+				"content_type": "image/png"
+			},
+			"show_ceiling": true,
+			"show_channel": 5,
+			"sort": 4,
+			"show_period_type": 1,
+			"time_periods": [{
+				"startDay": 1,
+				"endDay": 1,
+				"startTime": "00:00:00",
+				"endTime": "23:59:59",
+				"businessId": 113
+			}],
+			"link_type": 3,
+			"link_url": "/pages/activity/monday_deliver_free/index",
+			"self_app": true,
+			"is_link_wechat_status": false
+		}, {
+			"id": 129,
+			"content": "加入「阿喜熟客群」，领18元新人礼！",
+			"image_id": 769051,
+			"image": {
+				"id": 769051,
+				"path": "/storage/ad/2022/12/12/220a27479ce940809aabc353b2d1f1ef.jpg",
+				"url": "https://go.cdn.heytea.com/storage/ad/2022/12/12/220a27479ce940809aabc353b2d1f1ef.jpg",
+				"width": "144",
+				"height": "48",
+				"content_type": "image/png"
+			},
+			"show_ceiling": true,
+			"show_channel": 1,
+			"sort": 9,
+			"show_period_type": 1,
+			"time_periods": [{
+				"startDay": 1,
+				"endDay": 7,
+				"startTime": "00:00:00",
+				"endTime": "23:59:59",
+				"businessId": 129
+			}],
+			"link_type": 6,
+			"self_app": true,
+			"link_activity_id": 265,
+			"is_link_wechat_status": false
+		}, {
+			"id": 140,
+			"content": "♻️ 按需点单，简约适度，喜享美味！",
+			"image": {
+				"id": null,
+				"path": null,
+				"url": null,
+				"width": null,
+				"height": null,
+				"content_type": null
+			},
+			"show_ceiling": true,
+			"show_channel": 15,
+			"sort": 11,
+			"show_period_type": 1,
+			"time_periods": [{
+				"startDay": 1,
+				"endDay": 7,
+				"startTime": "00:00:00",
+				"endTime": "23:59:59",
+				"businessId": 140
+			}],
+			"link_type": 1,
+			"self_app": true,
+			"is_link_wechat_status": false
+		}],
+		"license": [{
+			"id": 1054243,
+			"path": "2023/09/25/tmp/ca54e2cb31834795835209a826cb68c8.jpg",
+			"url": "https://go.cdn.heytea.com/2023/09/25/tmp/ca54e2cb31834795835209a826cb68c8.jpg",
+			"width": "1507",
+			"height": "2167",
+			"content_type": "image/jpeg"
+		}, {
+			"id": 1054244,
+			"path": "2023/09/25/tmp/e61aeec1d1ae404b94a39fe2a891197d.jpg",
+			"url": "https://go.cdn.heytea.com/2023/09/25/tmp/e61aeec1d1ae404b94a39fe2a891197d.jpg",
+			"width": "1473",
+			"height": "2128",
+			"content_type": "image/jpeg"
+		}],
+		"attachments": []
+}
--- miniprogram/pages/order/index.js
+++ miniprogram/pages/order/index.js
@@ -1,35 +1,59 @@
-import shopList from "../../api/order/shop-list"
-Page({
-data:{
-  top:0,
-  buttonInfo:{
-    top:0,
-    right:0
-  },
-  currentActiveType:1,
-  shopList:[]
-},
-setActiveType(e){
-  const type=e.target.dataset.type
-  this.setData({
-    currentActiveType:type,
-  })
-  },
-  
-onLoad(){
- const info= wx.getMenuButtonBoundingClientRect() //获取胶囊的top值
- console.log(info)
- this.setData({
-  buttonInfo:info,
-  shopList:shopList
- })
-},
+import shopList from '../../api/order/shop-list';
+import noticeList from '../../api/order/notice';
+import bannerList from "../../api/order/banner"
+import categories from "../../api/order/categories"
 
-    onShow() {
-      if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 1,
-        });
-      }
+
+Page({
+  data: {
+    top: 0,
+    btnInfo: {
+      top: 0,
+      right: 0,
     },
+    currentActiveType: 1,
+    shopList: [],
+    currentSelectShopItem:{},
+    noticeList:[],
+    bannerList:[],
+    windowInfo:{},
+    visible:true,
+    categoriesList:[]
+  },
+  setActiveType(e) {
+    const type = e.target.dataset.type;
+    this.setData({
+      currentActiveType: type,
+    });
+  },
+
+  onLoad() {
+    const windowInfo = wx.getWindowInfo();
+    const info = wx.getMenuButtonBoundingClientRect(); //获取胶囊的top值
+    let _bannerList=bannerList.data.filter((item)=>{ 
+      return item.position=="product"
+    })
+    this.setData({
+      btnInfo: info,
+      shopList: shopList,
+      windowInfo,
+      currentSelectShopItem:shopList.data[0],
+      noticeList:noticeList.notice,
+      bannerList:_bannerList,
+      categoriesList:categories.categories
+    });
+  },
+  setVisible(){
+      this.setData({
+          visible:!this.data.visible
+      })
+  },
+
+  onShow() {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 1,
+      });
+    }
+  },
 });
