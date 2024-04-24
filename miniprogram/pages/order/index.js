93912a9eee55c970bafa6933c44e45323148b63a
Snake <snake@horizonpay.cn>
2024-04-24 17:45:48 +0800

    1

--- miniprogram/pages/order/index.js
+++ miniprogram/pages/order/index.js
@@ -21,7 +21,6 @@ Page({
     categoriesList:[],
     currentActiveCategories:null,
     productMenue:100,
-    animationData:{}
   },
   setActiveType(e) {
     const type = e.target.dataset.type;
@@ -41,13 +40,7 @@ Page({
     let _bannerList=bannerList.data.filter((item)=>{ 
       return item.position=="product"
     })
-    var animation = wx.createAnimation({
-      duration: 2000, // 动画持续时间
-      timingFunction: 'ease-out', // 动画的效果，可选值：linear、ease、ease-in、ease-in-out、ease-out、step-start、step-end
-      delay: 0, // 动画延迟时间
-      transformOrigin: '50% 50% 0', // 动画的原点，默认为元素中心点
-    })
-     animation.opacity(1).scale(1).step();
+   
     this.setData({
       btnInfo: info,
       shopList: shopList,
@@ -56,7 +49,6 @@ Page({
       noticeList:noticeList.notice,
       bannerList:_bannerList,
       categoriesList:categories.categories,
-      animationData:animation.export()
     });
     
     let query = wx.createSelectorQuery().in(this);
@@ -71,6 +63,25 @@ Page({
       })
   });
   },
+  scollerTopAction(e){
+    this.setData({
+      visible:true
+    },()=>{
+      const windowInfo = wx.getWindowInfo();
+    let query = wx.createSelectorQuery().in(this);
+    // 然后逐个取出navbar和header的节点信息
+    // 选择器的语法与jQuery语法相同
+    query.select('#xxx').boundingClientRect();
+    query.exec((res) => {
+      // 分别取出navbar和header的高度
+      let top = res[0].top;
+      this.setData({
+        productMenue:windowInfo.windowHeight-top
+      })
+   });
+    })
+  },
+
   setVisible(){
       this.setData({
           visible:!this.data.visible
@@ -80,8 +91,8 @@ Page({
     if(e.target.offsetTop>100){
       this.setData({
         visible:false
-      })
-     const windowInfo = wx.getWindowInfo();
+      },()=>{
+        const windowInfo = wx.getWindowInfo();
       let query = wx.createSelectorQuery().in(this);
       // 然后逐个取出navbar和header的节点信息
       // 选择器的语法与jQuery语法相同
@@ -89,11 +100,17 @@ Page({
       query.exec((res) => {
         // 分别取出navbar和header的高度
         let top = res[0].top;
-        console.log(windowInfo.windowHeight-top)
         this.setData({
           productMenue:windowInfo.windowHeight-top
         })
-    }); 
+    });
+      })
+      
+    }else{
+      console.log(11)
+      this.setData({
+        visible:true
+      })
     }
   },
 
