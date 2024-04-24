5d062314b032b85571f51912f88877aca97c167d
Snake <snake@horizonpay.cn>
2024-04-24 17:08:57 +0800

    add

--- miniprogram/pages/order/index.js
+++ miniprogram/pages/order/index.js
@@ -18,7 +18,10 @@ Page({
     bannerList:[],
     windowInfo:{},
     visible:true,
-    categoriesList:[]
+    categoriesList:[],
+    currentActiveCategories:null,
+    productMenue:100,
+    animationData:{}
   },
   setActiveType(e) {
     const type = e.target.dataset.type;
@@ -26,13 +29,25 @@ Page({
       currentActiveType: type,
     });
   },
-
+  setActiveCategoryies(e){
+    const id = e.target.dataset.id;
+    this.setData({
+      currentActiveCategories:id
+    })
+  },
   onLoad() {
     const windowInfo = wx.getWindowInfo();
     const info = wx.getMenuButtonBoundingClientRect(); //获取胶囊的top值
     let _bannerList=bannerList.data.filter((item)=>{ 
       return item.position=="product"
     })
+    var animation = wx.createAnimation({
+      duration: 2000, // 动画持续时间
+      timingFunction: 'ease-out', // 动画的效果，可选值：linear、ease、ease-in、ease-in-out、ease-out、step-start、step-end
+      delay: 0, // 动画延迟时间
+      transformOrigin: '50% 50% 0', // 动画的原点，默认为元素中心点
+    })
+     animation.opacity(1).scale(1).step();
     this.setData({
       btnInfo: info,
       shopList: shopList,
@@ -40,13 +55,46 @@ Page({
       currentSelectShopItem:shopList.data[0],
       noticeList:noticeList.notice,
       bannerList:_bannerList,
-      categoriesList:categories.categories
+      categoriesList:categories.categories,
+      animationData:animation.export()
     });
+    
+    let query = wx.createSelectorQuery().in(this);
+    // 然后逐个取出navbar和header的节点信息
+    // 选择器的语法与jQuery语法相同
+    query.select('#xxx').boundingClientRect();
+    query.exec((res) => {
+      // 分别取出navbar和header的高度
+      let top = res[0].top;
+      this.setData({
+        productMenue:windowInfo.windowHeight-top
+      })
+  });
   },
   setVisible(){
       this.setData({
           visible:!this.data.visible
       })
+  },
+  leftSwiperScollerAction(e){
+    if(e.target.offsetTop>100){
+      this.setData({
+        visible:false
+      })
+     const windowInfo = wx.getWindowInfo();
+      let query = wx.createSelectorQuery().in(this);
+      // 然后逐个取出navbar和header的节点信息
+      // 选择器的语法与jQuery语法相同
+      query.select('#xxx').boundingClientRect();
+      query.exec((res) => {
+        // 分别取出navbar和header的高度
+        let top = res[0].top;
+        console.log(windowInfo.windowHeight-top)
+        this.setData({
+          productMenue:windowInfo.windowHeight-top
+        })
+    }); 
+    }
   },
 
   onShow() {
